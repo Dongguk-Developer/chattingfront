e374b5391bed66c86e8b501c6b5b2405862a47a7
kimehyun <lh7721004@naver.com>
2024-11-09 12:37:33 +0900

    [UPDATE] 디자인 수정, api 연결

--- app/firstpage/page.js
+++ app/firstpage/page.js
@@ -59,15 +59,31 @@ export default function FirstPage() {
             }, 1000);
             return; // 페이지 이동 안됨
         }
-
-        // 입력 완료 클릭 시 메인페이지 이동
-        router.push('/MainPage');
+        console.log({
+            name: name,
+            age: age,
+            mbti:mbti,
+            job:job,
+            studyfield:interest
+          })
+        fetch("https://api.studyhero.kr/profile/update", {
+        // fetch("http://localhost:8090/profile/update", {
+            method: "POST",
+            body: JSON.stringify({
+              name: name,
+              age: age,
+              mbti:mbti,
+              job:job,
+              studyfield:interest
+            }),
+          })
+          .then((response) => {response.json();if(response.status==200){router.push('/');}})
+          .then((result) => {console.log(result);});
     };
-
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen base-100 relative">
-            {/* 오류 알림 */}
-            {showError && (
+        <>
+        {/* 오류 알림 */}
+        {showError && (
                 <div role="alert" className="alert alert-error mb-4 bg-error absolute top-0 left-1/2 transform -translate-x-1/2" style={{ zIndex: 100 }}>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -101,135 +117,121 @@ export default function FirstPage() {
                     <span className="text-error-content font-sans">나이가 잘못 입력되었습니다.</span>
                 </div>
             )}
-
-            {/* 이전페이지이동 */}
-            <div className="absolute top-1 left-1 mt-6 p-4 z-10"> 
-                <button onClick={handleBackButtonClick} className="block">
-                    <Image src="/pagenation.png" alt="이전 페이지로 이동" width={30} height={30} />
-                </button>
-            </div>
-
-            {/* 텍스트그룹 */}
-            <div className="absolute top-0 left-4 mt-6 p-4 flex flex-col items-start">
-                <div 
-                    className="text-xs font-sans font-medium" 
-                    style={{ color: "base-content", marginLeft: '16px', fontSize: '0.75rem' }}
-                >
-                    로그인
+            <div className="inline-flex flex-col justify-start min-h-screen base-100 relative w-full">
+                <div className="flex flex-row justify-start ml-4 mt-6">
+                    <button onClick={handleBackButtonClick} className="block">
+                        <Image src="/pagenation.png" alt="이전 페이지로 이동" width={30} height={30} />
+                    </button>
+                    <div className="flex flex-col">
+                        <div 
+                            className="text-xs font-sans font-medium" 
+                            style={{ color: "base-content", marginLeft: '16px', fontSize: '0.75rem' }}
+                        >로그인</div>
+                        <div 
+                            className="text-xs font-sans font-medium"
+                            style={{ color: "base-content", marginLeft: '16px', fontSize: '0.85rem' }} >초기설정</div>
+                    </div>
+                    <div className="flex flex-col"></div>
                 </div>
-                <div 
-                    className="text-xs font-sans font-medium"
-                    style={{ color: "base-content", marginLeft: '16px', fontSize: '0.85rem' }} >
-                    초기설정
-                </div>
-            </div>
-
-            {/* 프로필사진 추가 */}
-            <div className="absolute top-16 left-1/2 transform -translate-x-1/2 mt-8">
-                <div
-                    id="profile-picture"
+                <div className="flex flex-row justify-center">
+                    <div id="profile-picture"
                     className="mask mask-circle cursor-pointer bg-base-300 relative" // 색상 변경
                     style={{ width: '130px', height: '130px', objectFit: 'cover' }}
-                    onClick={handleProfilePictureClick}
-                >
+                    onClick={handleProfilePictureClick}>
                     {profilePicture ? (
                         <img 
                             src={profilePicture} 
                             alt="프로필 사진" 
                             className="mask mask-circle" 
                             style={{ width: '130px', height: '130px', objectFit: 'cover' }} 
-                        />
-                    ) : (
+                        />) : (
                         <div className="absolute inset-0 flex items-center justify-center text-base-content text-xs bg-base-300">
                             추가
-                        </div>
-                    )}
-                </div>
-                <input
-                    type="file"
-                    accept="image/*"
-                    ref={fileInputRef}
-                    onChange={handleFileChange}
-                    className="hidden"
-                />
-            </div>
-
-            {/* 프로필 사진 아래 카드 박스 */}
-            <div className="flex w-full flex-col border-opacity-50 mt-12">
-                <div className="card bg-base-200 rounded-box w-4/5 mx-auto mt-20 flex items-center justify-center h-auto"> {/* 색상 변경 */}
-                    {/* 카드 상단에 텍스트 박스 */}
-                    <div className="flex flex-col space-y-4 p-4">
-                    <input  
-                            type="text"
-                            placeholder="이름"
-                            className="input input-sm bg-base-300 text-base-content font-sans text-[0.65rem]" 
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <input
-                            type="text"
-                            placeholder="나이"
-                            className="input input-sm w-full bg-base-300 text-base-content font-sans text-[0.65rem]" 
-                            value={age}
-                            onChange={(e) => setAge(e.target.value)}
-                        />
-                        {/* MBTI 선택 */}
-                        <select 
-                            className="select select-sm w-full mb-4 bg-base-300 text-base-content font-sans text-[0.65rem]" 
-                            value={mbti}
-                            onChange={(e) => setMbti(e.target.value)}
-                        >
-                            <option disabled value="">MBTI</option>
-                            <option value="ENFP">ENFP</option>
-                            <option value="ENTP">ENTP</option>
-                            <option value="ESTJ">ESTJ</option>
-                            <option value="ESFJ">ESFJ</option>
-                            <option value="ENFJ">ENFJ</option>
-                            <option value="ENTJ">ENTJ</option>
-                            <option value="ESTP">ESTP</option>
-                            <option value="ESFP">ESFP</option>
-                            <option value="ISTJ">ISTJ</option>
-                            <option value="ISFJ">ISFJ</option>
-                            <option value="INFJ">INFJ</option>
-                            <option value="INTJ">INTJ</option>
-                            <option value="ISTP">ISTP</option>
-                            <option value="ISFP">ISFP</option>
-                            <option value="INFP">INFP</option>
-                            <option value="INTP">INTP</option>
-                        </select>
-                        {/* 직업 선택 */}
-                        <select
-                            className="select select-sm w-full mb-4 bg-base-300 text-base-content font-sans text-[0.65rem]" 
-                            value={job}
-                            onChange={(e) => setJob(e.target.value)}
-                        >
-                            <option disabled value="">직업</option>
-                            <option value="학생">학생</option>
-                            <option value="직장인">직장인</option>
-                            <option value="프리랜서">프리랜서</option>
-                        </select>
-                        {/* 관심 분야 선택 */}
-                        <select 
-                            className="select select-sm w-full mb-4 bg-base-300 text-base-content font-sans text-[0.65rem]" 
-                            value={interest}
-                            onChange={(e) => setInterest(e.target.value)}
-                        >
-                            <option disabled value="">관심 분야</option>
-                            <option value="프로그래밍">프로그래밍</option>
-                            <option value="디자인">디자인</option>
-                            <option value="게임 개발">게임 개발</option>
-                        </select>
-
+                        </div>)}
                     </div>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        ref={fileInputRef}
+                        onChange={handleFileChange}
+                        className="hidden"/>
+                </div>
+
+                    <div className="flex flex-col justify-center mt-12">
+                        <div className="mx-3.5 mt-20 flex items-center justify-center h-auto"> {/* 색상 변경 */}
+                            {/* 카드 상단에 텍스트 박스 */}
+                            <div className="card bg-base-200 rounded-box border-opacity-50 flex flex-col space-y-4 p-4 min-w-96">
+                                <input  
+                                    type="text"
+                                    placeholder="이름"
+                                    className="input input-sm bg-base-300 text-base-content font-sans text-[0.85rem] my-1" 
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="나이"
+                                    className="input input-sm w-full bg-base-300 text-base-content font-sans text-[0.85rem] my-1" 
+                                    value={age}
+                                    onChange={(e) => setAge(e.target.value)}
+                                />
+                                {/* MBTI 선택 */}
+                                <select 
+                                    className="select select-sm w-full mb-4 bg-base-300 text-base-content font-sans text-[0.85rem] my-1" 
+                                    value={mbti}
+                                    onChange={(e) => setMbti(e.target.value)}
+                                >
+                                    <option disabled value="">MBTI</option>
+                                    <option value="ENFP">ENFP</option>
+                                    <option value="ENTP">ENTP</option>
+                                    <option value="ESTJ">ESTJ</option>
+                                    <option value="ESFJ">ESFJ</option>
+                                    <option value="ENFJ">ENFJ</option>
+                                    <option value="ENTJ">ENTJ</option>
+                                    <option value="ESTP">ESTP</option>
+                                    <option value="ESFP">ESFP</option>
+                                    <option value="ISTJ">ISTJ</option>
+                                    <option value="ISFJ">ISFJ</option>
+                                    <option value="INFJ">INFJ</option>
+                                    <option value="INTJ">INTJ</option>
+                                    <option value="ISTP">ISTP</option>
+                                    <option value="ISFP">ISFP</option>
+                                    <option value="INFP">INFP</option>
+                                    <option value="INTP">INTP</option>
+                                </select>
+                                {/* 직업 선택 */}
+                                <select
+                                    className="select select-sm w-full mb-4 bg-base-300 text-base-content font-sans text-[0.85rem] my-1" 
+                                    value={job}
+                                    onChange={(e) => setJob(e.target.value)}
+                                >
+                                    <option disabled value="">직업</option>
+                                    <option value="학생">학생</option>
+                                    <option value="직장인">직장인</option>
+                                    <option value="프리랜서">프리랜서</option>
+                                </select>
+                                {/* 관심 분야 선택 */}
+                                <select 
+                                    className="select select-sm w-full mb-4 bg-base-300 text-base-content font-sans text-[0.85rem] my-1" 
+                                    value={interest}
+                                    onChange={(e) => setInterest(e.target.value)}
+                                >
+                                    <option disabled value="">관심 분야</option>
+                                    <option value="프로그래밍">프로그래밍</option>
+                                    <option value="디자인">디자인</option>
+                                    <option value="게임 개발">게임 개발</option>
+                                </select>
+
+                            </div>
+                        </div>
+                    </div>
+                <div className="flex flex-row justify-center">
+                    <button className="btn btn-wide mt-12 bg-primary text-primary-content font-sans font-extrabold" onClick={handleComplete}>
+                    입력 완료
+                    </button>
                 </div>
             </div>
-            {/* 완료 버튼 */}
-             <button 
-                 className="btn btn-wide mt-12 bg-primary text-primary-content font-sans font-extrabold" 
-                onClick={handleComplete}
-             >
-             입력 완료
-             </button>
-        </div>
-    );
+        </>
+    )
+    
 }
